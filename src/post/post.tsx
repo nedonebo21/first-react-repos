7e6172de572ed48f6ea72083815b8385faa18c37
nedonebo21 <cursed.void10007@gmail.com>
2025-04-16 00:03:25 +0400

    local history commit

--- src/post/post.tsx
+++ src/post/post.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 export const  Post = () => {
+    const sum = (a:number, b:number) => {
+        return a + b;
+    }
+
     return (
         <div>
-            <h2>Post33333</h2>
+            <h2>Post</h2>
+            <button onClick={()=> sum(5, 5)}>sum</button>
         </div>
     )
 }
