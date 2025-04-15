165520d339d319481b2ce9899e1b0d0a5dda3bbc
nedonebo21 <cursed.void10007@gmail.com>
2025-04-15 21:53:21 +0400

    commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Lorem ipsum dolor.</p>
-        <a
-          className="App-link"
-          href="https://reactjs-2.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          L earn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <p>Lorem ipsum dolor.</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs-2.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    L earn React
+                </a>
+            </header>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>5</p>
+        </div>
+    );
 }
 
 export default App;
